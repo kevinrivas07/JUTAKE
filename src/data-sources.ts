6af3a5d58eded8d2cd6e1bc47f55ae009ea9ac7a
Tatiana <tatianamontoya0428@gmail.com>
2025-05-22 23:28:20 -0500

    se ajusta packjson

--- src/data-sources.ts
+++ src/data-sources.ts
@@ -6,12 +6,8 @@ import { Loan } from "./entity/loan";
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT || "5432"),
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  synchronize: true, // true solo en desarrollo
+  url: process.env.DATABASE_URL, // ✅ Cambia esto
+  synchronize: false, // ⚠️ solo en desarrollo
   logging: false,
   entities: [User, Book, Loan],
   migrations: [],
